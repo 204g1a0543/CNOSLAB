e2f2521637d1a7f5dce9488d1acb086ee4bbc498
204g1a0543 <94950013+204g1a0543@users.noreply.github.com>
2022-08-30 11:56:09 +0530

    B Jaswanth Completed Sample.c with C source Code

--- oslaB/sAMPLE.C
+++ oslaB/sAMPLE.C
@@ -4,3 +4,8 @@ Title of the Experiment: CPU Scheduling Using FCFS Algorithm
 Date of Creation:30-08-2022
 Date of Execution:30-08-2022
 */
+// Source Code
+    #include <stdio.h>
+    int main(){
+    printf("Hello World\n");
+ }
