40f86add6c6fd0baa1afb5e50c464334170672fa
204g1a0543 <94950013+204g1a0543@users.noreply.github.com>
2022-10-11 11:47:20 +0530

    B.Jaswanth created Round Robin.c

---
+++ OSLAB/Round Robin.c
@@ -0,0 +1,6 @@
+/* Name Of the Candidate: B.Jashwanth
+   Roll No: 204G1A0543
+   Title of the Experiment: CPU Scheduling Using Round Robin Algorithm
+   Date of Creation: 11-10-2022
+   Date of Execution: 11-10-2022
+*/
